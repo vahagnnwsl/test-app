4a6571636e06dbdc023ac7d5ebd744af3254608e
vahagnnwsl <devvahagn@gmail.com>
2019-12-18 14:31:48 +0400

    Updates

---
+++ precache-manifest.f3de7434afd959d551f5c60fb91db289.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee1792fc372567549e201d64f4e8098",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "e053893f952b5e4ddf3c",
+    "url": "/test-app/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "4490c3a5a00f6d1201ff",
+    "url": "/test-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "e053893f952b5e4ddf3c",
+    "url": "/test-app/static/js/2.0671d4ef.chunk.js"
+  },
+  {
+    "revision": "4490c3a5a00f6d1201ff",
+    "url": "/test-app/static/js/main.c6fdb58f.chunk.js"
+  },
+  {
+    "revision": "65bf3cd874ceb987ed0e",
+    "url": "/test-app/static/js/runtime-main.8489a7b0.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test-app/static/media/persik.4e1ec840.png"
+  }
+]);
